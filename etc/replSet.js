066072bcd41903fd5e61decb8e89af78b6cacd88
Kohányi Róbert <kohanyi.robert@gmail.com>
2012-09-10 17:09:21 +0200

    Changing replSet.js file format from dos to unix.

--- etc/replSet.js
+++ etc/replSet.js
@@ -1,15 +1,15 @@
 var config = {
-  _id: "multi",
+  _id: "test",
   members: [
-    {_id: 0, host: "127.0.0.1:27018", priority: 1, tags: {notebook: "a"}},
-    {_id: 1, host: "127.0.0.1:27019", priority: 1, tags: {notebook: "b"}},
-    {_id: 2, host: "127.0.0.1:27020", priority: 2, tags: {notebook: "c"}}
+    {_id: 0, host: "127.0.0.1:27018", priority: 1, tags: {test: "27018"}},
+    {_id: 1, host: "127.0.0.1:27019", priority: 1, tags: {test: "27019"}},
+    {_id: 2, host: "127.0.0.1:27020", priority: 2, tags: {test: "27020"}}
   ],
   settings: {
     getLastErrorModes: {
-      one: {notebook: 1},
-      two: {notebook: 2},
-      three: {notebook: 3}
+      one: {test: 1},
+      two: {test: 2},
+      three: {test: 3}
     }
   }
 }
