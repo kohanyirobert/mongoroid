32c774cc93c57eedf3a28fdf3ec56ba7de04411a
Kohányi Róbert <kohanyi.robert@gmail.com>
2012-09-10 17:08:58 +0200

    Deleting old configuration files.

--- etc/multi-auth.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/env sh
-sed -i "s@# \(keyFile = .*\)@\1@" multi-{1,2,3}.cfg
-sed -i "s@# \(auth = .*\)@\1@" multi-{1,2,3}.cfg
--- etc/multi-rm-locks.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/env sh
-for i in {1,2,3}
-do
-  rm -vf multi-$i/mongod.lock
-done
